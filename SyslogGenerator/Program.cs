d9620e235efae2da1b5544cf5d796b5ac44b5d61
ChaeSeongSik <css5831@outlook.com>
2025-02-12 10:30:57 +0900

    Soluction and Project Init

---
+++ SyslogGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SyslogGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
